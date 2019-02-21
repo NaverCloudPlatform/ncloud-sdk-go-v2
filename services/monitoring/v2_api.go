170633f14dfc59aa0e56c2f67a51175b1e2b2775
chiman.jang <chiman.jang@navercorp.com>
2019-02-21 10:31:43 +0900

    handle unexpected messages of success

--- services/monitoring/v2_api.go
+++ services/monitoring/v2_api.go
@@ -89,14 +89,14 @@ func (a *V2ApiService) GetListMetrics(getListMetricsRequest *GetListMetricsReque
 	defer localVarHttpResponse.Body.Close()
 	bodyBytes, _ := ioutil.ReadAll(localVarHttpResponse.Body)
 
-	if !strings.Contains(string(bodyBytes), `{"error"`) {
+	if localVarHttpResponse.StatusCode >= 300 || (localVarHttpResponse.StatusCode < 300 && !strings.HasPrefix(string(bodyBytes), `{`)) {
+		return &successPayload, reportError("Status: %v, Body: %s", localVarHttpResponse.Status, bodyBytes)
+	}
+
+	if !strings.Contains(string(bodyBytes), `{"error"`) && strings.HasPrefix(string(bodyBytes), `{`) {
 		if err = json.Unmarshal(bodyBytes[bytes.IndexAny(bytes.Trim(bodyBytes, "{"), "{"):len(bodyBytes)-1], &successPayload); err != nil {
 			return &successPayload, err
 		}
-	}
-
-	if localVarHttpResponse.StatusCode >= 300 {
-		return &successPayload, reportError("Status: %v, Body: %s", localVarHttpResponse.Status, bodyBytes)
 	}
 
 
@@ -163,14 +163,14 @@ func (a *V2ApiService) GetMetricStatistics(getMetricStatisticsRequest *GetMetric
 	defer localVarHttpResponse.Body.Close()
 	bodyBytes, _ := ioutil.ReadAll(localVarHttpResponse.Body)
 
-	if !strings.Contains(string(bodyBytes), `{"error"`) {
+	if localVarHttpResponse.StatusCode >= 300 || (localVarHttpResponse.StatusCode < 300 && !strings.HasPrefix(string(bodyBytes), `{`)) {
+		return &successPayload, reportError("Status: %v, Body: %s", localVarHttpResponse.Status, bodyBytes)
+	}
+
+	if !strings.Contains(string(bodyBytes), `{"error"`) && strings.HasPrefix(string(bodyBytes), `{`) {
 		if err = json.Unmarshal(bodyBytes[bytes.IndexAny(bytes.Trim(bodyBytes, "{"), "{"):len(bodyBytes)-1], &successPayload); err != nil {
 			return &successPayload, err
 		}
-	}
-
-	if localVarHttpResponse.StatusCode >= 300 {
-		return &successPayload, reportError("Status: %v, Body: %s", localVarHttpResponse.Status, bodyBytes)
 	}
 
 
