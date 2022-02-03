7f998b2291db8e47f3c7924b005555b34f9344e2
jj41 <doonung41@gmail.com>
2022-02-03 18:50:46 +0900

    chore: specify version

--- services/vmysql/configuration.go
+++ services/vmysql/configuration.go
@@ -27,7 +27,7 @@ func NewConfiguration(apiKeys ...*ncloud.APIKey) *ncloud.Configuration {
 	cfg := &ncloud.Configuration{
 		BasePath:      "https://ncloud.apigw.ntruss.com/vmysql/v2",
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "vmysql//go",
+		UserAgent:     "vmysql/1.0.0/go",
 	}
 	if len(apiKeys) > 0 {
 		cfg.APIKey = apiKeys[0]
